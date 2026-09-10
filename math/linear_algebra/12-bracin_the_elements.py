bae63cb4863aa2d4aea379dc49149c79e7df49c5
L-nsamba <l.nsamba@alustudent.com>
2026-09-10 10:03:56 -0700

    task12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module perfoms element-wise math operations"""
+
+
+def np_elementwise(mat1, mat2):
+    """Performs element-wise addition, subtraction, multiplication, and division."""
+    mat1 = np.array(mat1)
+    mat2 = np.array(mat2)
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
