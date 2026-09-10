fe56f1bf5741d60ed9e22b68dbce59c43f1d838d
L-nsamba <l.nsamba@alustudent.com>
2026-09-10 09:13:13 -0700

    task3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module that returns the transpose of a 2D matrix."""
+
+
+def matrix_transpose(matrix):
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
