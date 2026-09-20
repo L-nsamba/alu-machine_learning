633e7502d4f6f426b9e1dc62bfc917fb50e18255
L-nsamba <l.nsamba@alustudent.com>
2026-09-20 02:23:54 -0700

    task 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 """ Module defines a function to calculate the determinant of a matrix """
 
+
 def determinant(matrix):
     """Calculation of the determinat of a matrix"""
+    if (not isinstance(matrix, list)
+        or not all(isinstance(row, list) for row in matrix)):
+    raise TypeError("matrix must be a list of lists")
+
     n = len(matrix)
     if n == 0 or (n == 1 and len(matrix[0]) == 0):
         return 1  # determinant of a 0x0 matrix is conventionally 1
